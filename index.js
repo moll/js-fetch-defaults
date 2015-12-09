c3c85d7ac50b697a2a0ecaaf7e607f363697edd6
Andri Möll <andri@dot.ee>
2015-12-09 23:35:33 +0200

    Parse root URL once.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-var resolve = require("url").resolve
+var Url = require("url")
 var assign = require("oolong").assign
 var merge = require("oolong").merge
+var PARSE_QUERY = false
+var PROTOCOL_RELATIVE = true // Enable //example.com/models to mimic browsers.
 
 exports = module.exports = function(fetch, rootUrl, defaults) {
-  if (typeof rootUrl !== "string") defaults = rootUrl, rootUrl = null
+  if (typeof rootUrl === "string") rootUrl = parseUrl(rootUrl)
+  else defaults = rootUrl, rootUrl = null
   return assign(exports.fetch.bind(null, fetch, rootUrl, defaults), fetch)
 }
 
 exports.fetch = function(fetch, rootUrl, defaults, url, opts) {
-  if (rootUrl != null) url = resolve(rootUrl, url)
+  if (rootUrl != null) url = rootUrl.resolve(url)
   if (typeof defaults === "function") defaults = defaults(url, opts)
   return fetch(url, opts == null ? defaults : merge({}, defaults, opts))
+}
+
+function parseUrl(url) {
+  return Url.parse(url, PARSE_QUERY, PROTOCOL_RELATIVE)
 }
