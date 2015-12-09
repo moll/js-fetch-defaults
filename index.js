03116e8d9ecbfa2cec0c29da55b6af3ec0a8a0f1
Andri Möll <andri@dot.ee>
2015-12-09 23:33:10 +0200

    Export fetch function.

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ var resolve = require("url").resolve
 var assign = require("oolong").assign
 var merge = require("oolong").merge
 
-module.exports = function(fetch, rootUrl, defaults) {
+exports = module.exports = function(fetch, rootUrl, defaults) {
   if (typeof rootUrl !== "string") defaults = rootUrl, rootUrl = null
+  return assign(exports.fetch.bind(null, fetch, rootUrl, defaults), fetch)
+}
 
-  return assign(function(url, opts) {
-    if (rootUrl != null) url = resolve(rootUrl, url)
-    if (typeof defaults === "function") defaults = defaults(url, opts)
-    return fetch(url, opts == null ? defaults : merge({}, defaults, opts))
-  }, fetch)
+exports.fetch = function(fetch, rootUrl, defaults, url, opts) {
+  if (rootUrl != null) url = resolve(rootUrl, url)
+  if (typeof defaults === "function") defaults = defaults(url, opts)
+  return fetch(url, opts == null ? defaults : merge({}, defaults, opts))
 }
